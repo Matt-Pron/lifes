5d09d7d7f01b8f77c1fa0b3c73da01c39f1adda1
Matt Pron <matiaspronzati@gmail.com>
2025-05-25 23:51:03 -0300

    25.5.25

--- src/render.cpp
+++ src/render.cpp
@@ -76,28 +76,28 @@ void WindowManager::drawHelpWin()
 
 void highlight(int indices)
 {
-    WINDOW** win = wm.getWin()
+    WINDOW** win = wm.getWin();
 
     wmove(win[0], 0, 0);
     for(int i = 0; i < entities.getPJSize(); i++)
     {
-        int maxx = getmaxx(pjs);
+        int maxx = getmaxx(win[0]);
         string hp = entities.getPJ(i, 0).c_str();
         int hpx = maxx - hp.length();
         if (i < 10){
-        mvwprintw(pjs, i+1, 2, "%d", i);
-        } else mvwprintw(pjs, i+1, 1, "%d", i);
-        mvwprintw(pjs, i+1, 4, "%s", entities.getPJ(i, 1).c_str());
-        mvwprintw(pjs, i+1, hpx, "%s", hp.c_str());
-        wrefresh(pjs);
+        mvwprintw(win[0], i+1, 2, "%d", i);
+        } else mvwprintw(win[0], i+1, 1, "%d", i);
+        mvwprintw(win[0], i+1, 4, "%s", entities.getPJ(i, 1).c_str());
+        mvwprintw(win[0], i+1, hpx, "%s", hp.c_str());
+        wrefresh(win[0]);
     }
-    for (int i = 0; i < wm.getPJSize(); i++)
+    for (int w = 0; w < entities.getPNJSize(); w++)
     {
-        if (i == indices)
+        if (w == indices)
             wattron(win[0], A_REVERSE);
-        mvwprintw(win[0], i+1, 1, pjsArr[i].c_str());
-        mvwprintw(win[0], i+1, 10, "16");
-        mvwprintw(win[0], i+1, 15, "12");
+        mvwprintw(win[0], w+1, 1, entities.getPJ(w, 1).c_str());
+        mvwprintw(win[0], w+1, 10, "16");
+        mvwprintw(win[0], w+1, 15, "12");
         wattroff(win[0], A_REVERSE);
     }
 }
